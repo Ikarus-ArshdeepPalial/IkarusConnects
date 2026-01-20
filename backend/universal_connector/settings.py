1a37c7417322d669064b41f8f03d079d867f8f0d
Ikarus-ArshdeepPalial <arshdeep.palial@ikarus3d.com>
2026-01-20 16:33:22 +0530

    Update settings.py

--- backend/universal_connector/settings.py
+++ backend/universal_connector/settings.py
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    f"https://{env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])}",
+]
 
 AUTH_USER_MODEL = 'user.User'
 
